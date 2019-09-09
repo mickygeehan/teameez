6989a8f2b11181d0adbb0f3631209fcc0f42337b
emicgee <michael.geehan@ericsson.com>
2019-09-09 22:34:30 +0100

    Added simple rouete to API gateway

---
+++ api-gateway/src/main/java/gateway/ApiGatewayApplication.java
@@ -0,0 +1,27 @@
+package gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                .path("/get")
+                .uri("http://httpbin.org:80"))
+                .build();
+    }
+
+
+}
