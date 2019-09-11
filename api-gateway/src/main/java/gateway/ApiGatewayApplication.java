da4cbc69cd9b586a991ef11ca72ef6aec8f8fc06
emicgee <michael.geehan@ericsson.com>
2019-09-11 13:12:03 +0100

    Added sample route to the gateway

--- api-gateway/src/main/java/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/gateway/ApiGatewayApplication.java
@@ -2,11 +2,19 @@ package gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
+@EnableConfigurationProperties(UriConfiguration.class)
+@RestController
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
@@ -14,14 +22,34 @@ public class ApiGatewayApplication {
     }
 
 
-    @Bean
-    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route(p -> p
-                .path("/get")
-                .uri("http://httpbin.org:80"))
-                .build();
+//    @Bean
+//    public RouteLocator myRoutes(RouteLocatorBuilder builder, UriConfiguration uriConfiguration) {
+//        String httpUri = uriConfiguration.getUrl();
+//        return builder.routes()
+//                .route(p -> p
+//                    .path("/get")
+//                    .uri(httpUri))
+//                .build();
+//    }
+
+    @RequestMapping("/fallback")
+    public Mono<String> fallback() {
+        return Mono.just("fallback");
     }
 
 
+}
+
+@ConfigurationProperties(prefix = "my")
+class UriConfiguration {
+
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
